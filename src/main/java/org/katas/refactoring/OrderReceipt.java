dcc86e23961cf403f5db0cf1b6dbd25608aa4cb8
liyue130 <liyue20@jd.com>
2018-01-13 11:21:15 +0800

    重构换行符等

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -8,6 +8,8 @@ package org.katas.refactoring;
  */
 public class OrderReceipt {
   private Order order;
+  private static final String CHAR_SPLIT = "\t";
+  private static final String LINE_SPLIT = "\n";
 
   public OrderReceipt(Order order) {
     this.order = order;
@@ -16,7 +18,7 @@ public class OrderReceipt {
   public String printReceipt() {
     StringBuilder output = new StringBuilder();
 
-    output.append("======Printing Orders======\n");
+    output.append("======Printing Orders======").append(LINE_SPLIT);
 
     // print date, bill no, customer name
     output.append(order.getCustomerName());
@@ -26,23 +28,24 @@ public class OrderReceipt {
     generatePrintItems(output);
 
     // prints the state tax
-    output.append("Sales Tax").append('\t').append(order.getTotalSalesTx());
+    output.append("Sales Tax").append(CHAR_SPLIT).append(order.getTotalSalesTx());
 
     // print total amount
-    output.append("Total Amount").append('\t').append(order.getTotalAmount());
+    output.append("Total Amount").append(CHAR_SPLIT).append(order.getTotalAmount());
     return output.toString();
   }
 
   private void generatePrintItems(StringBuilder output) {
+
     for (LineItem lineItem : order.getLineItems()) {
       output.append(lineItem.getDescription());
-      output.append('\t');
+      output.append(CHAR_SPLIT);
       output.append(lineItem.getPrice());
-      output.append('\t');
+      output.append(CHAR_SPLIT);
       output.append(lineItem.getQuantity());
-      output.append('\t');
+      output.append(CHAR_SPLIT);
       output.append(lineItem.totalAmount());
-      output.append('\n');
+      output.append(LINE_SPLIT);
     }
   }
 }
