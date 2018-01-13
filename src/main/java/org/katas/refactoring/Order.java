1f472174098384b888375481e6ff3400f13f9ced
liyue130 <liyue20@jd.com>
2018-01-13 10:53:43 +0800

    重构OrderReceipt

--- src/main/java/org/katas/refactoring/Order.java
+++ src/main/java/org/katas/refactoring/Order.java
@@ -7,10 +7,15 @@ public class Order {
   private final String address;
   private final List<LineItem> lineItems;
 
+  private double totalSalesTx = 0d;
+  private double totalAmount = 0d;
+
   public Order(String name, String address, List<LineItem> lineItems) {
     this.name = name;
     this.address = address;
     this.lineItems = lineItems;
+
+    calcSalesTax();
   }
 
   public String getCustomerName() {
@@ -23,5 +28,29 @@ public class Order {
 
   public List<LineItem> getLineItems() {
     return lineItems;
+  }
+
+  private void calcSalesTax(){
+
+    double salesTax = 0d;
+
+    for (LineItem lineItem: lineItems) {
+
+      salesTax = lineItem.totalAmount() * .10;
+      totalSalesTx += salesTax;
+
+      // calculate total amount of lineItem = price * quantity + 10 % sales tax
+      totalAmount += lineItem.totalAmount() + salesTax;
+
+    }
+
+  }
+
+  public double getTotalSalesTx(){
+    return this.totalSalesTx;
+  }
+
+  public double getTotalAmount() {
+    return totalAmount;
   }
 }
--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -5,53 +5,44 @@ package org.katas.refactoring;
  * price and amount. It also calculates the sales tax @ 10% and prints as part
  * of order. It computes the total order amount (amount of individual lineItems +
  * total sales tax) and prints it.
- * 
  */
 public class OrderReceipt {
-    private Order o;
+  private Order order;
 
-    public OrderReceipt(Order o) {
-        this.o = o;
-	}
+  public OrderReceipt(Order order) {
+    this.order = order;
+  }
 
-	public String printReceipt() {
-		StringBuilder output = new StringBuilder();
+  public String printReceipt() {
+    StringBuilder output = new StringBuilder();
 
-		// print headers
-		output.append("======Printing Orders======\n");
+    output.append("======Printing Orders======\n");
 
-		// print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
+    // print date, bill no, customer name
+    output.append(order.getCustomerName());
+    output.append(order.getCustomerAddress());
 
-		// prints lineItems
-		double totSalesTx = 0d;
-		double tot = 0d;
-		for (LineItem lineItem : o.getLineItems()) {
-			output.append(lineItem.getDescription());
-			output.append('\t');
-			output.append(lineItem.getPrice());
-			output.append('\t');
-			output.append(lineItem.getQuantity());
-			output.append('\t');
-			output.append(lineItem.totalAmount());
-			output.append('\n');
+    // prints lineItems
+    generatePrintItems(output);
 
-			// calculate sales tax @ rate of 10%
-            double salesTax = lineItem.totalAmount() * .10;
-            totSalesTx += salesTax;
+    // prints the state tax
+    output.append("Sales Tax").append('\t').append(order.getTotalSalesTx());
 
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            tot += lineItem.totalAmount() + salesTax;
-		}
+    // print total amount
+    output.append("Total Amount").append('\t').append(order.getTotalAmount());
+    return output.toString();
+  }
 
-		// prints the state tax
-		output.append("Sales Tax").append('\t').append(totSalesTx);
-
-        // print total amount
-		output.append("Total Amount").append('\t').append(tot);
-		return output.toString();
-	}
+  private void generatePrintItems(StringBuilder output) {
+    for (LineItem lineItem : order.getLineItems()) {
+      output.append(lineItem.getDescription());
+      output.append('\t');
+      output.append(lineItem.getPrice());
+      output.append('\t');
+      output.append(lineItem.getQuantity());
+      output.append('\t');
+      output.append(lineItem.totalAmount());
+      output.append('\n');
+    }
+  }
 }
